5d14739897fafc11e955ec144fd531ba25fd6aa1
Jan Stavel <jstavel@redhat.com>
2018-02-26 12:18:26 -0500

    added 'pong' message once a request appears

--- src/server.js
+++ src/server.js
@@ -7,15 +7,15 @@ const fileWatch = fs.watch(filename);
 const wss = new WebSocket.Server({port: 9091,
                                   path:"/monitor/etc/rhsm/rhsm.conf"});
 Rx.Observable.fromEvent(wss,'connection')
-    .flatMap((ws) => Rx.Observable.of({"socket": ws,
-                                       "event": "open",
-                                       "filename": filename,
-                                       "filecontent": fs.readFileSync(filename).toString('base64')})
-                                     .merge( Rx.Observable.fromEvent(fileWatch,'change')
-                                             .map((x) => { return {"socket": ws,
-                                                                   "event": "change",
-                                                                   "filename": filename,
-                                                                   "filecontent": fs.readFileSync(filename).toString('base64')}})))
+    .flatMap((ws) =>{
+	var msg = (name) => { return {"socket": ws,
+				      "event": name,
+				      "filename": filename,
+				      "filecontent": fs.readFileSync(filename).toString('base64')}};
+	return Rx.Observable.of(msg("open"))
+            .merge( Rx.Observable.fromEvent(fileWatch,'change').map((x) => { return msg("change") }))
+	    .merge( Rx.Observable.fromEvent(ws,'message').map((x) => { return msg("pong") }))
+    })
     .subscribe(
         (x) => {x.socket.send(JSON.stringify({"event":x.event,
                                               "file":x.filename,
@@ -28,6 +28,9 @@ Rx.Observable.fromEvent(wss,'connection')
 // wss.on('connection', function connection(ws) {
 //     const filename = "/etc/rhsm/rhsm.conf";
 //     ws.on('message', function incoming(message){
+//         ws.send(JSON.stringify({"event": event,
+//                                 "file-name": filename,
+//                                 "file-content": fs.readFileSync(filename).toString('base64')}));
 //     });
 //     fs.watch(filename, (event, fname) => {
 //         ws.send(JSON.stringify({"event": event,
