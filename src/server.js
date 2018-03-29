6264d50b56a0e2cd903bb43498d8c12803cee410
Jan Stavel <jstavel@redhat.com>
2018-03-29 11:18:35 +0000

    added rhsm/status service

--- src/server.js
+++ src/server.js
@@ -6,26 +6,24 @@ const { exec } = require ('child_process');
 const wss = new WebSocket.Server({port: 9091});
 
 // Rx.Observable streams accepts just one argument. 'connection' event provides two arguments. There is a pre-processing function to merge the args into an array
-var ConnectionsStream = Rx.Observable.fromEvent(wss,'connection', null, (ws,req) => [ws,req]).publish();
+var connectionsStream = Rx.Observable.fromEvent(wss,'connection', null, (ws,req) => [ws,req]).publish();
 
-ConnectionsStream.filter(([ws,req]) => req.url.match(/^\/monitor\/(.*)/))
+connectionsStream.filter(([ws,req]) => req.url.match(/^\/monitor\/(.*)/))
   .flatMap(([ws,req]) =>{
-
     let filename = req.url.replace(/^\/monitor\/+/,'/');
     let fileWatch = fs.watch(filename);
-
-  	var msg = (name) => {
+    var msg = (name) => {
       return [ws, {"time": (new Date()).toJSON(),
                    "event": name,
                    "file": filename,
                    "content":fs.readFileSync(filename).toString('base64')}];
     };
 
-  	return Rx.Observable.of(msg("open"))
-      .merge(Rx.Observable.fromEvent(ws,'message').map((x) => { console.log("monitor request appeared: ",x);
+    return Rx.Observable.of(msg("open"))
+	    .merge(Rx.Observable.fromEvent(ws,'message').map((x) => { console.log("monitor request appeared: ",x);
                                                                 return msg("pong"); }))
-      .merge(Rx.Observable.fromEvent(fileWatch,'change').map((x) => { console.log("a change of a filename happened");
-                                                                      return msg("change"); }));
+	    .merge(Rx.Observable.fromEvent(fileWatch,'change').map((x) => { console.log("a change of a filename happened");
+									                                                    return msg("change"); }));
   })
   .subscribe(
     ([ws,msg]) => {ws.send(JSON.stringify(msg));},
@@ -33,7 +31,7 @@ ConnectionsStream.filter(([ws,req]) => req.url.match(/^\/monitor\/(.*)/))
     () => {console.log('completed');}
   );
 
-ConnectionsStream.filter(([ws,req]) => req.url.match(/^\/execute\/(.*)/))
+connectionsStream.filter(([ws,req]) => req.url.match(/^\/execute\/(.*)/))
   .flatMap(([ws,req]) => {
     let cmd = req.url.replace(/^\/execute\/+/,'/');
   	var response = (name) => {
@@ -51,8 +49,8 @@ ConnectionsStream.filter(([ws,req]) => req.url.match(/^\/execute\/(.*)/))
                  .map((x) => [ws, Object.assign(response('run'),
                                                 {args: args,
                                                  error: x[0],
-						 stdout: x[1],
-						 stderr: x[2]})]);
+						                                     stdout: x[1],
+						                                     stderr: x[2]})]);
              }));
   })
   .subscribe(
@@ -61,68 +59,11 @@ ConnectionsStream.filter(([ws,req]) => req.url.match(/^\/execute\/(.*)/))
     () => console.log('completed')
   );
 
-ConnectionsStream.connect();
+const rhsmStatus = require('./services/rhsm/status.js')(connectionsStream);
+rhsmStatus.subscribe(
+  ([ws,msg]) => ws.send(JSON.stringify(msg)),
+  (err) => console.log('error: %s', err),
+  () => console.log('completed')
+);
 
-// wss.on('connection', (ws,req) => {
-//   Rx.Observable.of([ws,req])
-//     .flatMap(([ws,req]) =>{
-//   	  var msg = (name) => {
-//         console.log(req.url);
-//         return {"socket": ws,
-//   				      "time": (new Date()).toJSON(),
-//   				      "event": name};};
-//   	  return Rx.Observable.of(msg("open"))
-//   	    .merge( Rx.Observable.fromEvent(ws,'message').map((x) => { return msg("pong"); }));
-//     })
-//     .subscribe(
-//       (x) => {x.socket.send(JSON.stringify({"event":x.event,
-//                                             "time": x.time,
-//                                             "file":x.filename,
-//                                             "content":x.filecontent}));},
-//       (err) => {console.log('error: %s', err);},
-//       () => {console.log('completed');}
-//     );
-// });
-// Rx.Observable.fromEvent(wss,'connection')
-//   .flatMap((ws,req) =>{
-// 	  var msg = (name) => {
-//       console.log(req);
-//       console.log(ws);
-//       return {"socket": ws,
-// 				      "time": (new Date()).toJSON(),
-// 				      "event": name};};
-// 	  return Rx.Observable.of(msg("open"))
-// 	    .merge( Rx.Observable.fromEvent(ws,'message').map((x) => { return msg("pong"); }));
-//   })
-//   .subscribe(
-//     (x) => {x.socket.send(JSON.stringify({"event":x.event,
-//                                           "time": x.time,
-//                                           "file":x.filename,
-//                                           "content":x.filecontent}));},
-//     (err) => {console.log('error: %s', err);},
-//     () => {console.log('completed');}
-//   );
-
-// server.listen(9091,function(){
-//   console.log('Listening on %d', server.address().port);
-// });
-
-// Rx.Observable.fromEvent(wss,'connection')
-//     .flatMap((ws) =>{
-// 	    var msg = (name) => { return {"socket": ws,
-// 				                            "time": (new Date()).toJSON(),
-// 				                            "event": name,
-// 				                            "filename": filename,
-// 				                            "filecontent": fs.readFileSync(filename).toString('base64')};};
-// 	    return Rx.Observable.of(msg("open"))
-//         .merge( Rx.Observable.fromEvent(fileWatch,'change').map((x) => { return msg("change"); }))
-// 	      .merge( Rx.Observable.fromEvent(ws,'message').map((x) => { return msg("pong"); }));
-//     })
-//     .subscribe(
-//         (x) => {x.socket.send(JSON.stringify({"event":x.event,
-//                                               "time": x.time,
-//                                               "file":x.filename,
-//                                               "content":x.filecontent}));},
-//       (err) => {console.log('error: %s', err);},
-//       () => {console.log('completed');}
-//     );
+connectionsStream.connect();
