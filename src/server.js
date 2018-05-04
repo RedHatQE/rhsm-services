c5615a480f69dedb1a9c54fda5f6720e6e836691
root <root@centos7.subman.example.com>
2018-05-04 08:15:47 +0000

    refactor to use newer syntax style

--- src/server.js
+++ src/server.js
@@ -1,52 +1,65 @@
 const WebSocket = require('ws');
 const fs = require('fs');
-const Rx = require('rxjs/Rx');
 const { exec } = require ('child_process');
 
 const wss = new WebSocket.Server({port: 9091});
 
-// Rx.Observable streams accepts just one argument. 'connection' event provides two arguments. There is a pre-processing function to merge the args into an array
-var connectionsStream = Rx.Observable.fromEvent(wss,'connection', null, (ws,req) => [ws,req]).publish();
+import { Observable, pipe } from 'rxjs/Rx';
+import { filter, map, flatMap, reduce } from 'rxjs/operators';
+import { merge } from 'rxjs/observable/merge';
 
-connectionsStream.filter(([ws,req]) => req.url.match(/^\/monitor\/(.*)/))
-  .flatMap(([ws,req]) =>{
-    let filename = req.url.replace(/^\/monitor\/+/,'/');
-    let fileWatch = fs.watch(filename);
-    var msg = (name) => {
-      return [ws, {"time": (new Date()).toJSON(),
-                   "event": name,
-                   "file": filename,
-                   "content":fs.readFileSync(filename).toString('base64')}];
-    };
+// Observable streams accepts just one argument. 'connection' event provides two arguments. There is a pre-processing function to merge the args into an array
+var connectionsStream = Observable.fromEvent(wss,'connection', null, (ws,req) => [ws,req]).publish();
 
-    return Rx.Observable.of(msg("open"))
-	    .merge(Rx.Observable.fromEvent(ws,'message').map((x) => { console.log("monitor request appeared: ");
-                                                                return msg("pong"); }))
-	    .merge(Rx.Observable.fromEvent(fileWatch,'change').map((x) => { console.log("a change of a filename happened");
-									                                                    return msg("change"); }));
-  }).merge(
-	connectionsStream.filter(([ws,req]) => req.url.match(/^\/execute\/(.*)/))
-	  .flatMap(([ws,req]) => {
+merge(
+  connectionsStream.pipe(
+    filter(([ws,req]) => req.url.match(/^\/monitor\/(.*)/)),
+    flatMap(([ws,req]) =>{
+      let filename = req.url.replace(/^\/monitor\/+/,'/');
+      let fileWatch = fs.watch(filename);
+      var msg = (name) => {
+        return [ws, {"time": (new Date()).toJSON(),
+                     "event": name,
+                     "file": filename,
+                     "content":fs.readFileSync(filename).toString('base64')}];
+      };
+
+      return Observable.of(msg("open"))
+	      .merge(Observable.fromEvent(ws,'message').map((x) => { console.log("monitor request appeared: ");
+                                                               return msg("pong"); }))
+	      .merge(Observable.fromEvent(fileWatch,'change').map((x) => { console.log("a change of a filename happened");
+									                                                   return msg("change"); }));
+    })
+  ),
+	connectionsStream.pipe(
+    filter(([ws,req]) => req.url.match(/^\/execute\/(.*)/)),
+	  flatMap(([ws,req]) => {
 	    let cmd = req.url.replace(/^\/execute\/+/,'/');
 	  	var response = (name) => {
 	      return {"time": (new Date()).toJSON(),
 	              "event": name,
 	              "command": cmd};
 	    };
-	    return Rx.Observable.of([ws, response('hello, give me arguments to run the command')])
-	      .merge(Rx.Observable.fromEvent(ws,'message')
+	    return Observable.of([ws, response('hello, give me arguments to run the command')])
+	      .merge(Observable.fromEvent(ws,'message')
 	             .flatMap((x) => {
 	               let args = x.data;
 	               let actuallCmd = cmd + " " + args;
 	               console.log("a request arrived. cmd to execute:",actuallCmd);
-	               return Rx.Observable.bindCallback(exec, Array.of)(cmd + " " + x.data)
+	               return Observable.bindCallback(exec, Array.of)(cmd + " " + x.data)
 	                 .map((x) => [ws, Object.assign(response('run'),
 	                                                {args: args,
 	                                                 error: x[0],
 							                                     stdout: x[1],
 							                                     stderr: x[2]})]);
-	             }));
-  })).merge(require('./services/rhsm/status.js')(connectionsStream))
+	             })
+              );
+    })),
+  connectionsStream.pipe(
+    filter(([ws,req]) => req.url.match(/^\/rhsm\/status/)),
+    flatMap(require('./services/rhsm/status.js'))
+  )
+)
 .subscribe(
     ([ws,msg]) => ws.send(JSON.stringify(msg), (error) => {console.log('error when sending a response');
 							                                             console.log(error);}),
--- src/services/rhsm/status.js
+++ src/services/rhsm/status.js
@@ -1,5 +1,9 @@
 const { exec } = require('child_process');
-const Rx = require('rxjs/Rx');
+const fs = require('fs');
+
+import { Observable } from 'rxjs/Rx';
+import { merge } from 'rxjs/observable/merge';
+import { map, flatMap } from 'rxjs/operators';
 
 function RHSMStatusMsg(time,error,stdout,stderr){
   this.time = time;
@@ -9,14 +13,18 @@ function RHSMStatusMsg(time,error,stdout,stderr){
   this.overallStatus = stdout.match(/^Overall Status:([^\n]+)/m)[1].trim();
 }
 
-function rhsmStatus (connectionsStream){
+function rhsmStatus ([ws,req]){
   let getRHSMStatus = (ws) => {
-    return Rx.Observable.bindCallback(exec,Array.of)("/usr/bin/subscription-manager status")
-      .map((x) => new RHSMStatusMsg((new Date()).toJSON(), x[0], x[1], x[2]))
-      .map((x) => { return [ws,x]; });
-  };
-  return connectionsStream.filter(([ws,req]) => req.url.match(/^\/rhsm\/status/))
-    .flatMap(([ws,req]) => getRHSMStatus(ws).merge(
-      Rx.Observable.fromEvent(ws,'message').flatMap((x) => getRHSMStatus(ws))));
+    return Observable.bindCallback(exec,Array.of)("/usr/bin/subscription-manager status").pipe(
+      map((x) => new RHSMStatusMsg((new Date()).toJSON(), x[0], x[1], x[2])),
+      map((msg) => { return [ws,msg]; })
+    );};
+  let entitlementWatch = fs.watch('/etc/pki/entitlement');
+  return merge(getRHSMStatus(ws),
+               Observable.fromEvent(ws,'message').flatMap((x) => getRHSMStatus(ws)),
+               Observable.fromEvent(entitlementWatch,'change').pipe(
+                 map((x) => {console.log('a system entitlement status has been changed'); return x;}),
+                 flatMap((x) => getRHSMStatus(ws)))
+              );
 };
 module.exports = rhsmStatus;
